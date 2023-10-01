a89c6a0b7363b3842fa68a8692d50c898d38938e
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2023-10-01 18:00:04 -0300

    Finished chap 03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
+  const [feedbacks, setFeedbacks] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedbacks={feedbacks} />
+        <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+      </div>
+    </>
   );
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, in? Saepe molestiae ab excepturi quas eveniet? Harum eius error voluptatum voluptatem, porro, fugit, ab incidunt nihil dolores quibusdam necessitatibus maxime.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, in? Saepe molestiae ab excepturi quas eveniet? Harum eius error voluptatum voluptatem, porro, fugit, ab incidunt nihil dolores quibusdam necessitatibus maxime.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, in? Saepe molestiae ab excepturi quas eveniet? Harum eius error voluptatum voluptatem, porro, fugit, ab incidunt nihil dolores quibusdam necessitatibus maxime.",
+  },
+];
+
+export default FeedbackData;
